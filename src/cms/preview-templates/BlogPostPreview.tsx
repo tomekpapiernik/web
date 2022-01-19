bd7c7de528307b292fa96da4b3cc5f80ce3682c1
zeropaper <65971+zeropaper@users.noreply.github.com>
2022-01-19 12:07:58 +0100

    fix: load styles in CMS
    related to https://github.com/ory-corp/marketing/issues/5

--- src/cms/preview-templates/BlogPostPreview.tsx
+++ src/cms/preview-templates/BlogPostPreview.tsx
@@ -1,6 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Location } from '@reach/router'
+
+import 'prismjs'
+import 'prismjs/components/prism-bash'
+import 'prismjs/components/prism-go'
+import 'prismjs/components/prism-javascript'
+import 'prismjs/components/prism-jsx'
+import 'prismjs/components/prism-pug'
+import 'prismjs/components/prism-shell-session'
+import 'prismjs/components/prism-tsx'
+import 'prismjs/components/prism-typescript'
+import 'prismjs/components/prism-yaml'
+
+import '../../../gatsby-browser'
+
 import { PlainBlogTemplate } from '../../templates/blog'
 
 const BlogPostPreview = ({ entry, widgetFor }: any) => {
