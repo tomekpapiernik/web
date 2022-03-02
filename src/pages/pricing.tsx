0f2ffe7d63f919e50ebed025a3366a8fe8e17509
jfcurran <john@ory.sh>
2022-03-02 15:26:40 +0100

    set padded to true on banner.tsx inside pricing.tsx

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -7,17 +7,17 @@ import Layout from '../components/layouts/layout/layout'
 import Pricing from '../components/layouts/pricing/pricing'
 import SEO from '../components/layouts/seo/seo'
 import * as content from '../page-content/content-pricing'
-import { pricingCloud } from '../page-content/pricing-tiers/pricing-cloud'
-import { sharedBannerSlack } from '../page-content/shared/content-banner'
+import {pricingCloud} from '../page-content/pricing-tiers/pricing-cloud'
+import {sharedBannerSlack} from '../page-content/shared/content-banner'
 
 const PricingPage = () => (
-  <Layout>
-    <SEO {...content.seo} />
-    <Pricing {...content.pricing} {...pricingCloud} />
-    <Banner {...sharedBannerSlack} />
-    <FeatureList {...content.featurelist} />
-    <Faq {...content.faq} />
-  </Layout>
+    <Layout>
+        <SEO {...content.seo} />
+        <Pricing {...content.pricing} {...pricingCloud} />
+        <Banner padded={true} {...sharedBannerSlack} />
+        <FeatureList {...content.featurelist} />
+        <Faq {...content.faq} />
+    </Layout>
 )
 
 export default PricingPage
